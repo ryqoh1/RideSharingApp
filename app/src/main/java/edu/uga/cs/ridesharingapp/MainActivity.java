45fb04b4a98e9bc63fff89af5cb28b13d266cea4
Nhan Tran <ntt21786@uga.edu>
2024-04-22 20:26:26 -0400

    Fragment Implementation/Gradle changes

--- app/src/main/java/edu/uga/cs/ridesharingapp/MainActivity.java
+++ app/src/main/java/edu/uga/cs/ridesharingapp/MainActivity.java
@@ -4,21 +4,39 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int id = item.getItemId();
+            if (id == R.id.navigation_home) {
+                selectedFragment = new HomeFragment();
+            } else if (id == R.id.navigation_ride_offers) {
+                selectedFragment = new RideOffersFragment();
+            } else if (id == R.id.navigation_ride_requests) {
+                selectedFragment = new RideRequestsFragment();
+            } else if (id == R.id.navigation_account) {
+                selectedFragment = new AccountFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            }
+            return true;
         });
+
+        // Set initial fragment to avoid null
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+        }
     }
 }
