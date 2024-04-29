29dcd72bab82384e40375e19790cd144d6838fe5
liamfenn <lgf63569@uga.edu>
2024-04-28 20:19:20 -0400

    Firebase imports

--- app/src/main/java/edu/uga/cs/ridesharingapp/RideRequestsFragment.java
+++ app/src/main/java/edu/uga/cs/ridesharingapp/RideRequestsFragment.java
@@ -8,6 +8,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class RideRequestsFragment extends Fragment {
 
     @Nullable
